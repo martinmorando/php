34f5f141650d78a190e046a61ff0d1116ad7007e
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-08 15:01:55 -0300

    replace double with float, add more operators

--- numbers.php
+++ numbers.php
@@ -2,9 +2,28 @@
 /*
 	Numbers
 */
-$integer = 1;
-$double = 2.33;
+$a = 1; // Integer
+$b = 2.33; // Float
+$c = $a * $b; // Float
 
-echo "Integer: " . $integer . "\n";
-echo $integer * $double; // Prints double
+if (is_int($a)) {
+	echo $a . " is an integer.\n";
+}
+
+if (is_float($b)) {
+	echo $b . " is a float.\n";
+}
+
+
+# Increment and decrement operators
+$a++;
+$a--;
+
+
+# Compound assignment operators
+$a += $b; // $a = $a + $b;
+$a *= $b;
+$a /= $b;
+$a %= $b;
+$a **= $b; // $a = $a ** $b;
 ?>
