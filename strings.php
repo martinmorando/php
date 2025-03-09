c6088ffa8e9db4f1801c722159a9642bc8388b3b
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-09 11:41:10 -0300

    experiment with whitespace adjustments for better visibility on GH

--- strings.php
+++ strings.php
@@ -16,15 +16,15 @@ echo $message;
 
 // Some built-in functions related to strings
 $open_secret = "Bitcoin";
-echo "\nConvert to lowercase: "			. strtolower($open_secret);
-echo "\nConvert to uppercase: "			. strtoupper($open_secret);
-echo "\nCapitalize (1st char): "		. ucfirst($open_secret);	
-echo "\nLength: "						. strlen($open_secret);
-echo "\nReverse : "						. strrev($open_secret);
-echo "\nRepeat n times: "				. str_repeat($open_secret, 21);
-echo "\nCount substring in string: "	. substr_count($open_secret, "coin");
-echo "\nStrip surrounding whitespace: "	. trim($open_secret);
-echo "\nStrip whitespace to the left: "	. ltrim($open_secret);
+echo "\nConvert to lowercase: "         . strtolower($open_secret);
+echo "\nConvert to uppercase: "         . strtoupper($open_secret);
+echo "\nCapitalize (1st char): "        . ucfirst($open_secret);	
+echo "\nLength: "                       . strlen($open_secret);
+echo "\nReverse : "                     . strrev($open_secret);
+echo "\nRepeat n times: "               . str_repeat($open_secret, 21);
+echo "\nCount substring in string: "    . substr_count($open_secret, "coin");
+echo "\nStrip surrounding whitespace: " . trim($open_secret);
+echo "\nStrip whitespace to the left: " . ltrim($open_secret);
 echo "\nStrip whitespace to the right: ". rtrim($open_secret); 
 echo "\n";
 ?>
