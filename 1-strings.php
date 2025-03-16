16a73c5836f68139fa1b9650a787f338c3bf4eed
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-16 18:19:13 -0300

    add str_word_count(), strpos() and str_replace() to 1-strings.php

--- 1-strings.php
+++ 1-strings.php
@@ -1,6 +1,6 @@
 <?php
 /*
-	Strings
+    Strings
 */
 
 // Formatting
@@ -20,11 +20,14 @@ echo "\nConvert to lowercase: "         . strtolower($open_secret);
 echo "\nConvert to uppercase: "         . strtoupper($open_secret);
 echo "\nCapitalize (1st char): "        . ucfirst($open_secret);	
 echo "\nLength: "                       . strlen($open_secret);
+echo "\nCount words: "                  . str_word_count($open_secret);
 echo "\nReverse : "                     . strrev($open_secret);
-echo "\nRepeat n times: "               . str_repeat($open_secret, 21);
+echo "\nRepeat string n times: "        . str_repeat($open_secret, 21);
+echo "\nFind position of substring: "   . strpos($open_secret, "t");
 echo "\nCount substring in string: "    . substr_count($open_secret, "coin");
+echo "\nReplace substring with substring: " . str_replace("Bi", "Sh1", $open_secret);
 echo "\nStrip surrounding whitespace: " . trim($open_secret);
 echo "\nStrip whitespace to the left: " . ltrim($open_secret);
-echo "\nStrip whitespace to the right: ". rtrim($open_secret); 
+echo "\nStrip whitespace to the right: ". rtrim($open_secret);
 echo "\n";
 ?>
